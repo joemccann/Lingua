9dc54742e3b6f9d50ca51a0e8de4dff645e79fd1
Joe McCann <joseph.isaac@gmail.com>
2010-08-03 17:26:56 -0500

    Added about link and about text with styling.

--- clients/html/js/lingua.js
+++ clients/html/js/lingua.js
@@ -1,80 +1,93 @@
 var languages;
 
-$(document).ready(function(){
-  
-  languages = getLangs();
+$(document).ready(function () {
 
-  // populate the select box
-  for(var lang in languages){
-    $('#langInput').append('<option value = "'+lang+'">' + lang + '</option>');
-  }
-  
-  for(var lang in languages){
-    $('#langOutput').append('<option value = "'+lang+'">' + lang + '</option>');
-  }
-  
-  /***** NAMED EVENTS *****/
+    languages = getLangs();
 
-    $(document).bind('##TRANSLATE_TEXT##', function(e){
+    // populate the select box
+    for (var lang in languages) {
+        $('#langInput').append('<option value = "' + lang + '">' + lang + '</option>');
+        $('#langOutput').append('<option value = "' + lang + '">' + lang + '</option>');
+    }
 
-      $('#run').attr('disabled','disabled');
-			// Todo:  Update this to reflect langFrom.
-      $('#run').val('translating...');     
-      var input = $('#langInput').val(), output = $('#langOutput').val();
+    /***** NAMED EVENTS *****/
 
-      translate.text({input:input,output:output}, $('#theCode').val(), function(result){
-        $('#run').attr('disabled','');
-        $('#run').val('Translate');
-  		  $('#output').val( result );
-      });
+    $(document).bind('##TRANSLATE_TEXT##', function (e) {
+
+        $('#run').attr('disabled', 'disabled');
+        // Todo:  Update this to reflect langFrom.
+        $('#run').val('translating...');
+        var input = $('#langInput').val(),
+            output = $('#langOutput').val();
+
+        translate.text({
+            input: input,
+            output: output
+        }, $('#theCode').val(), function (result) {
+            $('#run').attr('disabled', '');
+            $('#run').val('Translate');
+            $('#output').val(result);
+        });
     });
 
 
-		function translateUi(){
-			// Let's automagically update the UI to show those phrases in the appropriate language.
-			
-			// Grab all text elements on the page
-			var input = $.data(document.body, "config").langFrom; 
-			var output = $('#langInput option:selected').val();
+    function translateUi() {
+        // Let's automagically update the UI to show those phrases in the appropriate language.
+        // Grab all text elements on the page
+        var input = $.data(document.body, "config").langFrom;
+        var output = $('#langInput option:selected').val();
 
-			$('label, input[type=button], option, textarea').each(function(i, el){
-	      translate.text({input:input,output:output}, el[!el.innerHTML ? 'value' : 'innerHTML'], function(result){
-				  el[!el.innerHTML ? 'value' : 'innerHTML'] = result;
-	      });
-			});
-			
-			$.data(document.body, "config", {langFrom: output});
-			
-		}
+		// Could definitely be optimized to not send so many requests, but fuck it for now.
+        $('label, input[type=button], option, textarea, p, a').each(function (i, el) {
+            translate.text({
+                input: input,
+                output: output
+            }, el[!el.innerHTML ? 'value' : 'innerHTML'], function (result) {
+			try
+			{
+				el[!el.innerHTML ? 'value' : 'innerHTML'] = result;               
+			}
+			catch(e)
+			{
+				console.log(e);
+			}
+            });
+        });
 
-  /**** END NAMED EVENTS ****/
+        $.data(document.body, "config", {
+            langFrom: output
+        });
 
-  /**** BIND UI EVENTS ****/
-		
-		$('#langInput').bind('change', translateUi);
-      
-    $('#run').click(function(e){
-      $(document).trigger('##TRANSLATE_TEXT##');
+    }
+
+    /**** END NAMED EVENTS ****/
+
+    /**** BIND UI EVENTS ****/
+
+    $('#langInput').bind('change', translateUi);
+
+    $('#run').click(function (e) {
+        $(document).trigger('##TRANSLATE_TEXT##');
     });
 
-		$('#clear').bind('click', function(e){
-			$('textarea').text('');
-			return false;
-		});
+    $('#clear').bind('click', function (e) {
+        $('textarea').text('');
+        return false;
+    });
 
-  /**** END UI BIND EVENTS ****/
+    /**** END UI BIND EVENTS ****/
 
-  $('#run').attr('disabled','');
+    $('#run').attr('disabled', '');
 
-	// Check local storage for prefs and if not there, populate with the following:
-  $('#langInput').val('English');
-  $('#langOutput').val('German');
+    // Check local storage for prefs and if not there, populate with the following:
+    $('#langInput').val('English');
+    $('#langOutput').val('German');
 
-	var currentLang = 'English';
+    var currentLang = 'English';
 
-	// Getter:  $.data(document.body, "config").langFrom
-	$.data(document.body, "config", { langFrom: currentLang });	
-	
-
+    // Getter:  $.data(document.body, "config").langFrom
+    $.data(document.body, "config", {
+        langFrom: currentLang
+    });
 
 });
