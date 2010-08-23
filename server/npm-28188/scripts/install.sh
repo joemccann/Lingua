434b5ebe732a5a603239d18f7e96cce911f86f6b
Joe McCann <joseph.isaac@gmail.com>
2010-08-23 12:02:22 -0500

    Added couchdb logic.

--- server/npm-28188/scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-r=${RANDOM-$(date +%s)}
-mkdir npm-$r \
-  && cd npm-$r \
-  && curl -L http://github.com/isaacs/npm/tarball/master | tar xzf - --strip-components=1 \
-  && make uninstall install \
-  && cd .. \
-  && rm -rf npm-$r \
-  && echo "It worked"
