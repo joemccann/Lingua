4d242139d9eddad46fa595fcc9264104bf3f0e11
Joe McCann <joseph.isaac@gmail.com>
2010-09-21 07:07:05 -0500

    removed lingua original android folder. Updated some html for main screen.

--- server/app.js
+++ server/app.js
@@ -15,28 +15,33 @@ var express = require('express'),
     app = express.createServer();
 
 // Configuration
-app.configure(function () {
+app.configure(function () 
+{
     app.set('views', __dirname + '/views');
     app.use('/', connect.bodyDecoder());
     app.use('/', connect.methodOverride());
     app.use(connect.staticProvider(__dirname + '/public'));
 });
 
-app.configure('development', function () {
+app.configure('development', function () 
+{
     app.use(connect.errorHandler({
         dumpExceptions: true,
         showStack: true
     }));
 });
 
-app.configure('production', function () {
+app.configure('production', function () 
+{
     app.use(connect.errorHandler());
 });
 
 
-app.get('/store', function (req, res) {
+app.get('/store', function (req, res) 
+{
 
-    storeInCouch(req, function (revision) {
+    storeInCouch(req, function (revision) 
+    {
         res.send(revision);
     });
 
@@ -57,37 +62,43 @@ var dbId = 'lingua-couch', // eventually pass enviornment variables and set defa
 
 
 // Initialize vars relative to couchdb instance.
-request({
+request(
+{
     uri: uri
-}, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+}, function (error, response, body) 
+{
+    if (!error && response.statusCode == 200) 
+    {
         //sys.puts(sys.inspect(body)) // If you need to see it...
-        console.log('Initial call to couchone successful.')
+        console.log('\nInitial call to couchone successful.\n')
         doc = JSON.parse(body);
         rev = doc._rev;
 
         // Now kick off some YQL action to build up our database.
-        yqlNyTimes();
+        //yqlNyTimes();
 
 		// If we passed in an interval from the command line then, call it on that interval.
         yqlInterval && setInterval(yqlNyTimes, yqlInterval);
     }
-    else {
+    else 
+    {
         assert.equal(response.statusCode, 200);
     }
 });
 
 // Helper method for building couchone uri.
-function buildUri(host, db, id) {
-    return host + db + id;
-}
+function buildUri(host, db, id) { return host + db + id; }
 
 
 // Update the document with the new data object.
 // Fire callback if it exits.
-function updateDoc(obj, cb) {
-    request(obj, function (error, response, body) {
-        if (error) {
+function updateDoc(obj, cb) 
+{
+    request(obj, function (error, response, body) 
+    {
+        if (error) 
+        {
+        	// Throw an error if you want.
             //throw new Error(error);
             cb(response.statusCode);
             sys.puts(response.statusCode + " instead of an error.\n");
@@ -101,8 +112,8 @@ function updateDoc(obj, cb) {
 }
 
 // Runs various checks to properly format the data for storage in couchone.
-
-function storeInCouch(req, cb) {
+function storeInCouch(req, cb) 
+{
 
     // TODO: Sanitize this
     var compoundKey = req.query.from.toLowerCase() + "_" + req.query.to.toLowerCase();
@@ -110,27 +121,35 @@ function storeInCouch(req, cb) {
     var words = [];
     words = req.query.message.split(" ");
     var firstword = words[0].toLowerCase();
+    
+    
+    // TODO: use the words and create random phrases to further beef up db of words and phrases.
 
     // Does compoundKey exist?
-    if (typeof doc[compoundKey] === 'undefined') {
+    if (typeof doc[compoundKey] === 'undefined') 
+    {
         // Create a new entry
         console.log('Adding a new compoundkey: ' + compoundKey);
         doc[compoundKey] = {};
 
         doc[compoundKey][firstword] = [];
-        doc[compoundKey][firstword][0] = {
+        doc[compoundKey][firstword][0] = 
+        {
             "from": req.query.message,
             "to": req.query.output,
             "timestamp": new Date()
         };
     }
-    else {
+    else 
+    {
         // Does the firstword key exist?
-        if (typeof doc[compoundKey][firstword] === 'undefined') {
+        if (typeof doc[compoundKey][firstword] === 'undefined') 
+        {
             console.log('The firstword is not there so we are adding a new one.')
             // TODO: create new firstword key.
             doc[compoundKey][firstword] = [];
-            doc[compoundKey][firstword][0] = {
+            doc[compoundKey][firstword][0] = 
+            {
                 "from": req.query.message,
                 "to": req.query.output,
                 "timestamp": new Date()
@@ -141,18 +160,22 @@ function storeInCouch(req, cb) {
             var messageExists = false;
             var index = -1;
 
-            doc[compoundKey][firstword].forEach(function (el, i, a) {
-                if (el.from === req.query.message) {
+            doc[compoundKey][firstword].forEach(function (el, i, a) 
+          	{
+                if (el.from === req.query.message) 
+                {
                     messageExists = true;
                     index = i;
                 }
             });
 
-            if (messageExists) {
+            if (messageExists) 
+            {
                 console.log("Updating the timestamp since the message, " + req.query.message + ", exists.")
                 doc[compoundKey][firstword][index].timestamp = new Date();
             }
-            else {
+            else 
+            {
                 // add the new message, output and timestamp
                 console.log("Adding a new message.")
                 doc[compoundKey][firstword].push({
@@ -168,7 +191,8 @@ function storeInCouch(req, cb) {
     doc._rev = rev;
 
 	// Build data object (record).
-    var updateHash = {
+    var updateHash = 
+    {
         uri: uri,
         method: 'PUT',
         body: JSON.stringify(doc)
@@ -186,15 +210,14 @@ function processCouchStack()
     var cur = couchStack.length === 0 ? null : couchStack.pop();
 
 	// Are we done?
-    if (!cur) {
+    if (!cur) 
+    {
         console.log('We are finished adding to couchone.');
         return;
     }
 
     // Pass to storeInCouch
-    storeInCouch(cur, function () {
-        processCouchStack(); 		// mmmm, async....
-    });
+    storeInCouch(cur, function () { processCouchStack() });      // mmmm, async....
 
 }
 
@@ -216,14 +239,15 @@ function translateYql(el, i)
         sys.puts(resp + '\n');
 
         // Create formatted hash for couchdb as it is expecting something similar to a http request.
-        var hash = {
-            'query': {
+        var hash = 
+        {
+            'query': 
+            {
                 'from': langFrom,
                 'to': langTo,
                 'message': el.title,
                 'output': resp
             }
-
         }
         
         // Add to stack (state machine if you will).
@@ -237,9 +261,11 @@ function translateYql(el, i)
 
 
 // Pull the headlines from the NY Times RSS feed with YQL.
-function yqlNyTimes() {
+function yqlNyTimes() 
+{
 
-    request({
+    request(
+    {
         uri: yql
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -253,10 +279,10 @@ function yqlNyTimes() {
         else {
             assert.equal(response.statusCode, 200);
         }
-    })
+    });
 }
 
 
 app.listen(3001);
 
-console.log('Express server started on port %s', app.address().port);
+console.log('\nExpress server started on port %s', app.address().port);
